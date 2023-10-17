e9b32a6d675834010ce0357f39e64b8e8ae80a5f
Bon <bonoshea@gmail.com>
2023-10-17 15:09:17 +0100

    Added extra api endpoints. Replaced background and added logo

--- app/api/ApiConfig.js
+++ app/api/ApiConfig.js
@@ -9,11 +9,15 @@ export const LOGOUT = BASE_URL + "logout";
 export const CHANGE_PASSWORD = BASE_URL + "change-password";
 export const LIST_PRODUCT = BASE_URL + "products";
 export const UPDATE_PROFILE = BASE_URL + "update-profile";
-
-
-
-
-
-
-
-
+export const CART_ADD = BASE_URL + "cart/add";
+export const TIMESLOT = BASE_URL + "timeslots/byevent";
+export const CART_VIEW = BASE_URL + "cart/view";
+export const CART_REMOVE = BASE_URL + "cart/remove";
+export const CHECKOUT = BASE_URL + "cart/checkout";
+export const VIEW_ORDERS = BASE_URL + "view_orders";
+export const VIEW_ORDER = BASE_URL + "view_order";
+export const RESERVE = BASE_URL + "reserve";
+export const RELEASE = BASE_URL + "release";
+export const TIMESLOT_BY_EVENT = BASE_URL + "timeslots/byevent";
+export const FEATURED_LIST = BASE_URL + "featured";
+export const RELEASE_ALL = BASE_URL + "release_all";
